59f2ee5b272d7850c16626fb4411c3f7beea8c3c
dkormann <dominikormann@outlook.com>
2024-04-11 14:07:17 +0200

    mnist works

--- mnist_model.py
+++ mnist_model.py
@@ -1,11 +1,8 @@
 #%%
-
 import torch 
 from torchvision import datasets, transforms
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-
-
 
 # Download and load the MNIST training dataset
 train_dataset = datasets.MNIST(root='./data', train=True, download=True)
@@ -18,9 +15,7 @@ train_targets = train_dataset.targets
 train_data = train_data.unsqueeze(1).float() / 255.0
 train_targets = torch.tensor(train_targets)
 
-
 #%%
-
 i=5
 print(train_targets[i])
 plt.imshow(train_data[i,0], cmap='gray')
@@ -30,8 +25,8 @@ class Model(torch.nn.Module):
   
   def __init__(self):
     super().__init__()
-    self.W = torch.randn(784, 10) / 28
-    self.b = torch.zeros(10)
+    self.W = torch.nn.Parameter(torch.randn(784, 10) / 28)
+    self.b = torch.nn.Parameter(torch.zeros(10))
     
   def forward(self, x):
     x = x.view(x.shape[0], -1)
@@ -39,18 +34,32 @@ class Model(torch.nn.Module):
     return x
 
 model = Model()
-opt = torch.optim.SGD([model.b, model.W], lr=1e-2)
+opt = torch.optim.Adam([model.b, model.W], lr=1e-2)
 #%%
-prediction = model(train_data)
 
 def loss_fn(p, y):
   p = p.softmax(dim=1)
   l = F.cross_entropy(p, y)
   return l
 
+#%%
+def step():
+  prediction = model(train_data)
+  l = loss_fn(prediction, train_targets)
+  opt.zero_grad()
+  l.backward()
+  opt.step()
+  return l.item()
 
-l = loss_fn(prediction, train_targets)
+#%%
+for i in range(100):
+  print(step())
+  
+#%%
 
-opt.zero_grad()
-# l.backward()
-# opt.step()
+prediction = model(train_data)
+#%%
+k = 4
+plt.plot(prediction[k].softmax(-1).detach())
+plt.show()
+plt.imshow(train_data[k,0])
